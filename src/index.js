abf72b24c4909c608733057dc34487686f05c34e
kaylawoodbury <kaylasandoval92@gmail.com>
2020-04-04 17:38:16 +0200

    updated base urls to heroku

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "semantic-ui-css/semantic.min.css";
 import { Provider } from "react-redux";
 import configureStore from "./state/store/configureStore";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://newsroom-team-1.herokuapp.com/api";
 
 const store = configureStore();
 window.store = store;
